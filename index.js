e4f8f6b0d3190c75920a9ebab6e1d4f28acbb586
José Eduardo <jose.es.oleg@gmail.com>
2023-06-23 20:17:13 -0300

    Finaliza sign-up

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = 5000
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const users = [];
 const tweets = [];
@@ -35,10 +36,18 @@ app.post("/tweets", (req, res) => {
 app.post("/sign-up", (req, res) => {
   const {username, avatar} = req.body
 
-  if(typeof username !== string || typeof avatar !== string || username.length=== 0 || avatar.length === 0){
+  if(typeof username !== "string" || typeof avatar !== "string" || username.length=== 0 || avatar.length === 0){
     return res.status(400).send("Todos os campos são obrigatórios!")
   }
   if(users.filter(u => u.name === username).length > 0) {
     return res.status(409).send('Usuário já cadastrado!')
   }
+
+  const newUser = {
+    username, avatar
+  }
+  
+  users.push(newUser);
+  
+  res.status(201).send("OK")
 } )
