8cf0f1cc0a726cea99721b53d465b20ee354544c
José Eduardo <jose.es.oleg@gmail.com>
2023-06-23 18:04:01 -0300

    versão em andamento

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 import express from 'express';
 import cors from 'cors';
 
-const PORT = 4001
+const PORT = 5000
 
 const app = express();
-app.use(cors())
-app.listen(`${PORT}`)
+app.use(cors());
 
-app.get("/tweets", () => {
+const users = [];
+const tweets = [];
+
+app.listen(`${PORT}`, () => {
   console.log(` Servidor rodando na porta: ${PORT}`)
+})
+
+app.get("/tweets", (req, res) => {
+
+} )
+app.post("/tweets", (req, res) => {
+  const {username, tweet} = req.body
+
+  if(users.length === 0){ 
+    return res.status(401).send("Por favor, faça login novamente")
+  }
+
+  const newTweet = {
+    username, tweet
+  }
+  tweets.push(newTweet)
+  return res.status(201).send("OK")
+
+} )
+
+
+app.post("/sign-up", (req, res) => {
+  const {username, avatar} = req.body
+
+  if(typeof username !== string || typeof avatar !== string || username.length=== 0 || avatar.length === 0){
+    return res.status(400).send("Todos os campos são obrigatórios!")
+  }
+  if(users.filter(u => u.name === username).length > 0) {
+    return res.status(409).send('Usuário já cadastrado!')
+  }
 } )
